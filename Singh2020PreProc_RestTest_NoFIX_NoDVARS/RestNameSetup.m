d08766593311201fd8e21c84ac63dcddd7ccdffc
rq-Chen <crqricky24@gmail.com>
2023-06-06 18:01:16 -0500

    Delete folders

--- Singh2020PreProc_RestTest_NoFIX_NoDVARS/RestNameSetup.m
+++
@@ -1,9 +0,0 @@
-function tseries = RestNameSetup(DataType)
-    if strcmp( DataType, 'HCP' )%if it's HCP Data Setup these folders
-        tseries = {'rfMRI_REST1_LR', 'rfMRI_REST1_RL',...
-                   'rfMRI_REST2_LR', 'rfMRI_REST2_RL'}
-    else%if the data is DMCC 
-        tseries = {'rfMRI_RestBas1_AP', 'rfMRI_RestBas2_PA', 'rfMRI_RestPro1_AP', ...
-            'rfMRI_RestPro2_PA', 'rfMRI_RestRea1_AP','rfMRI_RestRea2_PA' }
-    end
-end
